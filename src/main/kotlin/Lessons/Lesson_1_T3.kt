c6d318e459cdefd2d550bafe24b1e8389dc3276b
isarychev <balu20000@gmail.com>
2025-09-18 22:43:10 +0300

    KS-L1-T3

---
+++ src/main/kotlin/Lessons/Lesson_1_T3.kt
@@ -0,0 +1,13 @@
+package org.example.Lessons
+
+fun main() {
+    val yearOfFlight : Int = 1962
+    var hourOfFlight : Int = 9
+    var minuteOfFlight : Int = 7
+     println(yearOfFlight)
+     println(hourOfFlight)
+     println(minuteOfFlight)
+    hourOfFlight = 10
+    minuteOfFlight = 55
+     println("Boarding time $hourOfFlight:$minuteOfFlight")
+}
