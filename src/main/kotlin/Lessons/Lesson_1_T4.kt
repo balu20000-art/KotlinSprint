4de4881e438f3108b6e76e6a2b6b4a8176a147d3
isarychev <balu20000@gmail.com>
2025-09-18 23:02:09 +0300

    KS-L1-T4

---
+++ src/main/kotlin/Lessons/Lesson_1_T4.kt
@@ -0,0 +1,18 @@
+package org.example.Lessons
+
+fun main() {
+   val length : Long = 40868600000
+   val age : Short = 27
+   val partOfDay : Double = 0.075
+   val seconds : Short = 6480
+   val partOfYear : Double = 2.0547945205479453E-4
+   val apogee : Int = 327000
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+
+}
