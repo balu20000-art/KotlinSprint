ea3a05b7e5d4b2f121c89a8974e5413711c2662b
isarychev <balu20000@gmail.com>
2025-09-18 22:23:30 +0300

    KS-L1-T2

---
+++ src/main/kotlin/Lessons/Lesson_1.kt
@@ -0,0 +1,14 @@
+package org.example.Lessons
+
+fun main() {
+    val numberOforders: Int = 75
+    val thanksOfpuschase : String = "Excellent product !!"
+      println(numberOforders)
+      println(thanksOfpuschase)
+    var numberOfworkers : Int =2000
+      //println(numberOfworkers)
+      numberOfworkers = numberOfworkers - 1
+      println(numberOfworkers)
+
+
+}
