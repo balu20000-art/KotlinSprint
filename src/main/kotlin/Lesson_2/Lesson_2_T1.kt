e410237f5a9b4a9cbd3e80580974efcb01ad2888
isarychev <balu20000@gmail.com>
2025-09-19 14:19:24 +0300

    KS-L2-T1

---
+++ src/main/kotlin/Lesson_2/Lesson_2_T1.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson_2
+
+fun main() {
+    val numberOfAll = 4
+    val estimation1 : Double= 3.0
+    val estimation2 : Double = 4.0
+    val estimation3 : Double = 3.0
+    val estimation4: Double = 5.0
+    val averageScore  = ( estimation1 + estimation2 + estimation3 + estimation3 ) / numberOfAll
+      println(averageScore)
+}
