c2450414970e925cd74b1fefaa2ab82e002fdf99
isarychev <balu20000@gmail.com>
2025-09-21 16:13:57 +0300

    KS-1-3-create-task3-corrected

--- src/main/kotlin/Lesson_1/Lesson_1_T3.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_T3.kt
@@ -1,13 +1,13 @@
 package org.example.Lesson_1
 
 fun main() {
-    val yearOfFlight : Short = 1961
-    var hourOfFlight : Short = 9
-    var minuteOfFlight : Short = 7
-       println(yearOfFlight)
-       println(hourOfFlight)
-       println(minuteOfFlight)
+    val yearOfFlight: Short = 1961
+    var hourOfFlight: Short = 9
+    var minuteOfFlight: Short = 7
+    println(yearOfFlight)
+    println("0$hourOfFlight")
+    println("0$minuteOfFlight")
     hourOfFlight = 10
     minuteOfFlight = 55
-       println("$hourOfFlight:$minuteOfFlight")
+    println("$hourOfFlight:$minuteOfFlight")
 }
