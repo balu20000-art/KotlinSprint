689854c49560bc46ef2dd71ab3e36434d8fe4005
isarychev <balu20000@gmail.com>
2025-09-21 16:59:47 +0300

    KS-1-4-create-task4

---
+++ src/main/kotlin/Lesson_1/Lesson_1_T4.kt
@@ -0,0 +1,20 @@
+package org.example.Lesson_1
+
+fun main() {
+    val lenght: Long = 40868600000
+    val age: Short = 27
+    val partOfDay: Float = 0.075f
+    val sekonds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    print(
+        """
+         |$lenght
+         |$age
+         |$partOfDay
+         |$sekonds
+         |$partOfYear
+         |$apogee
+     """.trimMargin()
+    )
+}
