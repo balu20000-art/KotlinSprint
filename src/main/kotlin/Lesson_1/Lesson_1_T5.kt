217919965f709d0fc86a1721644bf5372439dffc
isarychev <balu20000@gmail.com>
2025-09-22 23:39:59 +0300

    KS-1-5-create-task5-corrected

--- src/main/kotlin/Lesson_1/Lesson_1_T5.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_T5.kt
@@ -3,9 +3,10 @@ package org.example.Lesson_1
 fun main() {
 
     val secondsAll = 6480
-    val secondsOfFlight = secondsAll % 60
-    val minuteOfFlight = secondsAll / 60 % 60
-    val hourOfFlight = secondsAll / 60 / 60
-    println("0$hourOfFlight:$minuteOfFlight:0$secondsOfFlight")
-
+    var secondsOfFlight = String.format("%02d", secondsAll % SEKOND_IN_MINUTE_IN_HOUR)
+    val minuteOfFlight = String.format("%02d", secondsAll / SEKOND_IN_MINUTE_IN_HOUR % SEKOND_IN_MINUTE_IN_HOUR)
+    val hourOfFlight = String.format("%02d", secondsAll / SEKOND_IN_MINUTE_IN_HOUR / SEKOND_IN_MINUTE_IN_HOUR)
+    println("$hourOfFlight:$minuteOfFlight:$secondsOfFlight")
 }
+
+const val SEKOND_IN_MINUTE_IN_HOUR = 60
